65cc35de26ed93c95f9d9483c8c4e556e9e08846
Joshua Buhler <josh@buhler.me>
2020-04-04 20:18:29 -0600

    Notes on install

--- Sources/SerialPi/main.swift
+++ Sources/SerialPi/main.swift
@@ -1,5 +1,11 @@
 import SerialPiCore
 
+// https://www.swiftbysundell.com/articles/building-a-command-line-tool-using-the-swift-package-manager/
+// Installing:
+// - swift build -c release
+// - cd .build/release
+// - cp -f SerialPi /usr/local/bin/serialpi
+
 let pi = SerialPi()
 
 do {
