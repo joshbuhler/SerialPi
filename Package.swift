7e3eb6ab51db4795bccfee139f20bd209ab3b258
Joshua Buhler <josh@buhler.me>
2020-04-04 20:09:39 -0600

    Adds Files dependency

--- Package.swift
+++ Package.swift
@@ -6,16 +6,22 @@ import PackageDescription
 let package = Package(
     name: "SerialPi",
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(
+            name: "Files",
+            url: "https://github.com/johnsundell/files.git",
+            from: "4.0.0"
+            )
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "SerialPi",
-            dependencies: ["SerialPiCore"]),
-        .target(name: "SerialPiCore"),
+            dependencies: ["SerialPiCore"]
+            ),
+        .target(name: "SerialPiCore",
+            dependencies: ["Files"]
+            ),
         .testTarget(
             name: "SerialPiTests",
             dependencies: ["SerialPi"]),
