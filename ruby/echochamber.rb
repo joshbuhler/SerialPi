a840ab8fea05038d7d503a8bb37ec8f22a5b850c
Joshua Buhler <josh@buhler.me>
2020-04-12 16:38:55 -0600

    sloppy keep running

--- ruby/echochamber.rb
+++ ruby/echochamber.rb
@@ -9,16 +9,23 @@ puts("♦️ Starting echochamber")
 # while line=STDIN.gets do
 # 	puts ("from ruby: #{line}")
 # end
-
+keepRunning = true
+while keepRunning do
 outputStr = ""
 while line=STDIN.getc do
 	outputStr << line
 	#puts ("from ruby: #{line}")
 
 	# puts("ruby: #{outputStr}")
+	if (outputStr == "quit")
+		puts("quitting\n")
+		keepRunning = false
+	end
+
 	if (line == "\n")
 		puts("♦️ endOfLine: #{outputStr}")
 		break
 	end
 end
-puts("♦️ done")
+#puts("♦️ done")
+end
