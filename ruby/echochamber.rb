dcfdaad57678a19bc43a6c649ad2adda3ab22ccc
Joshua Buhler <josh@buhler.me>
2020-04-12 20:48:50 -0600

    Keeping ruby awake

--- ruby/echochamber.rb
+++ ruby/echochamber.rb
@@ -1,14 +1,6 @@
 puts("♦️ Starting echochamber")
 
-# Open3.popen3("sleep 2; ls") do |stdin, stdout, stderr, thread|
-# 	while line=stdout.gets do
-# 		puts ("line: #{line}")
-# 	end
-# end
-
-# while line=STDIN.gets do
-# 	puts ("from ruby: #{line}")
-# end
+# This will keep the script running until it's killed.
 keepRunning = true
 while keepRunning do
 outputStr = ""
@@ -18,7 +10,7 @@ while line=STDIN.getc do
 
 	# puts("ruby: #{outputStr}")
 	if (outputStr == "quit")
-		puts("quitting\n")
+		puts("♦️ quitting\n")
 		keepRunning = false
 	end
 
